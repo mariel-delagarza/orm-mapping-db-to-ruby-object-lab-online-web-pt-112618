b4ede2ea1d696397c5a674af2dd5a6c7c81fff5b
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-18 16:24:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,11 +73,11 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-  end 
+  end
 
   def self.students_below_12th_grade
 
-  end 
+  end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)

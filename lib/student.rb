0844d78a9b2f6fe213b22ac5c0f6cade7d2158e3
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-18 16:03:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_student = self.new 
+    new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.length = row[2]
@@ -11,28 +11,28 @@ class Student
 
   def self.all
     sql = <<-SQL
-      SELECT * FROM students 
+      SELECT * FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end     
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
